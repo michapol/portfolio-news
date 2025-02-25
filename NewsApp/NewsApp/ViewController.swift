a1806c055b53efe0d01b60c44672c0d4402a4b0c
Mike Pollard <mike@michapol.com>
2025-02-25 13:12:23 +0000

    First commit - project

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Mike Pollard on 25/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
---
+++ NewsApp/NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Mike Pollard on 25/02/2025.
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
