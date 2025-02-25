fdedf3f61986aa92aff6d60acdfd906221b93412
Mike Pollard <mike@michapol.com>
2025-02-25 13:48:49 +0000

    Added in NewsService with basic test

--- NewsApp/NewsApp/ViewController.swift
+++ NewsApp/NewsApp/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Mike Pollard on 25/02/2025.
 //
 
+import NewsService
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,5 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        Task {
+            await testNewsService()
+        }
+    }
+
+    private func testNewsService() async {
+        let newsService = NewsService(apiKey: "<APIKEY>")
+        guard let data = try? await newsService.getHeadlines(for: .technology) else { return }
+        guard let string = String(data: data, encoding: .utf8) else { return }
+
+        print(string)
     }
 }
